3238154eb239ad8251e2e38960b9eb419bf7718f
김수진 <sjkim910115@gmail.com>
2020-10-06 22:49:27 +0900

    second commit

--- askacademy/settings.py
+++ askacademy/settings.py
@@ -31,16 +31,26 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third apps
+    'django_extensions',
+    'debug_toolbar',
+
+    # locals apps
     'blog1',
+    'instagram',
+
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +130,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 파일을 읽을 때
+MEDIA_URL = '/media/'
+# 파일을 저장할 때
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
+
+# 기본
+AUTH_USER_MODEL = 'auth.User'
