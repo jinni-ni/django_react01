596e11c681a71fcbaf4e18c86c52b7edc240054b
김수진 <sjkim910115@gmail.com>
2020-10-14 22:36:40 +0900

    로그인 커스터마이징

--- accounts/models.py
+++ accounts/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=6)
+    zipcode = models.CharField(max_length=6) #, validators=[])
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views.generic import TemplateView, UpdateView
+from accounts.models import Profile
+from .forms import ProfileForm
+
+
+# @login_required
+# def profile(request):
+#     return  render(request,'accounts/profile.html')
+
+class ProfileView(LoginRequiredMixin,TemplateView):
+    template_name = 'accounts/profile.html'
+
+profile = ProfileView.as_view()
+
+# class ProfileUpdateView(UpdateView):
+#     model = Profile
+#     form_class = ProfileForm
+
+@login_required
+def profile_edit(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = None
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'accounts/profile_form.html', {
+        'form': form,
+    })
+
+def signup(request):
+    pass
+
+def logout(request):
+    pass
+
